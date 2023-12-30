65f8a7f52710afebf3cff7ef436a72fd9a73b6fa
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2023-12-30 10:56:43 +0200

    craet Costum Note p1

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -7,11 +7,51 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16),
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-          SizedBox(height: 27,),
-          CostumAppbar()
+          SizedBox(
+            height: 35,
+          ),
+          CostumAppbar(),
+          NoteItem()
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              'build your carer with ramy',
+              style: TextStyle(color: Colors.black),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                )),
+          ),
+        Text('21may, 2024',style: TextStyle(color: Colors.black),)
         ],
       ),
     );
