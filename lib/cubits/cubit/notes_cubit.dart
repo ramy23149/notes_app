c7042ff26e758f9c092ab7f4be510ee718e34642
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2023-12-31 20:09:15 +0200

    add logic to notes cubit

--- lib/cubits/cubit/notes_cubit.dart
+++ lib/cubits/cubit/notes_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constans/constant.dart';
 import 'package:notes_app/cubits/cubit/notes_state.dart';
-
-
+import 'package:notes_app/models/note_modle.dart';
 
 class NotesCubit extends Cubit<AddNoteState> {
   NotesCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    try {
+      var notesBox = Hive.box(kNotesBox);
+
+      await notesBox.add(note);
+      emit(AddNoteLoading());
+    } catch (e) {
+      AddNoteFailer(e.toString());
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constans/constant.dart';
+import 'package:notes_app/models/note_modle.dart';
 import 'package:notes_app/views/notes_%20view.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(NoteModelAdapter());
 
-await Hive.openBox(kNotesBox);
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
