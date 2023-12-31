77c246eb98e86102b50482a167fcdce0e09a02e9
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2023-12-31 09:14:39 +0200

    create tybe adabtor for node model

--- lib/models/note_modle.dart
+++ lib/models/note_modle.dart
@@ -1,8 +1,17 @@
-class NoteModel {
-  final String title;
-  final String subTitle;
-  final int color;
+import 'package:hive/hive.dart';
 
+part 'note_modle.g.dart';
+
+@HiveType(typeId: 0)
+
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subTitle;
+  @HiveField(2)
+  final int color;
+  @HiveField(3)
   final String date;
 
   NoteModel(
