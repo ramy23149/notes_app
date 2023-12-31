6f8130e3485e56b3736b48adb9408077a34233d8
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2023-12-31 09:23:51 +0200

    init hive and open box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constans/constant.dart';
 import 'package:notes_app/views/notes_%20view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
@@ -12,7 +17,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: const NotesView(),
     );
   }
